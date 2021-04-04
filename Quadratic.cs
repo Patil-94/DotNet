00608861242c5a1f78513bd5a4bd6fa31e103de4
Patil-94 <ankitakhairnar10@.com>
2021-04-04 23:31:10 +0530

    [Ankita] Add Quadratic.cs file

---
+++ Quadratic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicDemo
+{
+    class Quadratic
+    {
+        /// <summary>
+        /// find root of Quadratic equation 
+        /// </summary>
+        public static void ReadQuadratic()
+        {
+            Console.WriteLine("Enter the value of a");   //enter input
+            int a = Convert.ToInt32(Console.ReadLine());  //convert to int with the help of convrt.ToInt32()
+            Console.WriteLine("Enter the value of b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of c");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            double delta = b * b - (4 * a * c);      
+            double sqr = Math.Sqrt(delta);
+            double root1 = (-b + sqr) / (2 * a);     
+            double root2 = (-b - sqr) / (2 * a);
+            Console.WriteLine("the value of root1" + root1);   //print output
+            Console.WriteLine("the value of root2" + root2);
+
+
+        }
+    }
+}
