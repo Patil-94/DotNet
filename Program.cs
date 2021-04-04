14e4c2958f19950aa67f69c388b34ece6ecc2d65
Patil-94 <ankitakhairnar10@.com>
2021-04-04 23:44:07 +0530

    [Ankita] Add program.cs file

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicDemo
+{
+    class Program
+    {
+        /// <summary>
+        /// Entry point for the console application
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // ReplaceString.ReadInput();
+
+
+            //FlipCoin flip = new FlipCoin();
+            // flip.ReadInput();
+
+
+            //LeapYear leap = new LeapYear();
+            //leap.ReadInput();
+
+
+            //PowerOfTwo.ReadInput();
+
+
+            //HarmonicProblem.ReadHarmonicNumber();
+
+
+            //MultiDimentional.ReadMultiArray();
+
+
+            //SumOfTriplet.FindTriplet();
+            //SumOfTriplet.FindTripletswithHashSet();
+
+
+            //PrimeFactorization.Findfactor();
+
+
+            // Gambler.ReadStakeAmountAndGoal();
+
+
+            //Distance.ReadDistance();
+
+            //  Windchill.ReadTemp();
+
+
+            //Quadratic.ReadQuadratic();
+
+            //Coupons.ReadNumOfCoupons();
+
+            //VendigMachine.ReadAmountToFindchange();
+
+            // Stopwatch.calElapsedTime();
+
+            Console.Read();
+        }
+    }
+}
