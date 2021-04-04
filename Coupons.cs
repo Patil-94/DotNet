acddcf23048713290e7301ae012c89e02fd00aeb
Patil-94 <ankitakhairnar10@.com>
2021-04-04 23:37:50 +0530

    [Ankita] Add Coupons.cs file

---
+++ Coupons.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicDemo
+{
+    class Coupons
+    {
+        public static void ReadNumOfCoupons()
+        {
+            Console.WriteLine("Enter the num for distinct coupons");
+            int couponNo = Convert.ToInt32(Console.ReadLine());
+            FindOutRandomCount(couponNo); //calling Findoutrandom() method
+        }
+        private static void FindOutRandomCount(int CouponNo)
+        {
+            int distinct = 0, count = 0;
+            bool[] iscollected = new bool[CouponNo];
+            while (distinct < CouponNo)
+            {
+                Random random = new Random();
+                int newRandom = Convert.ToInt32(random.Next(0, CouponNo));
+                count++;
+                if (!iscollected[newRandom])
+                {
+                    distinct++;
+                    iscollected[newRandom] = true;
+                }
+            }
+            Console.WriteLine("total random number needed to have all distict coupons:" + count);
+        }
+    }
+}
+
+
+
+
