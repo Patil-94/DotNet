adb3565cdeca3d68786f7f4a2f0d7b7f52b39415
Patil-94 <ankitakhairnar10@.com>
2021-04-04 23:36:12 +0530

    [Ankita] Add Relacestring.cs file

---
+++ ReplaceString.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicDemo
+{
+    class ReplaceString
+    {
+        /// <summary>
+        /// Reading input from user
+        /// </summary>
+        public static void ReadInput()
+        {
+            //given string template
+            string template = "Hello <<UserName>>, How are you?";
+            //given instruction/message to the user
+            Console.WriteLine("please enter your name");
+            //Reading input from user
+            string userInput = Console.ReadLine();
+            //calling replace() method
+            Replaceword(template, userInput);
+        }
+        /// <summary>
+        /// created replace() method to perform replacement
+        /// </summary>
+        /// <param name="template"></param>
+        /// <param name="UserInput"></param>
+        private static void Replaceword(string template, string UserInput)
+        {
+            //using predefined string Replace() method to perform replace opration
+            string result = template.Replace("<<UserName>>", UserInput);
+            //finally displaying updated template/statement
+            Console.WriteLine("After done replace " + result);
+        }
+
+    }
+}
+
+
+
